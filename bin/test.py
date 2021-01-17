350e8b91427a18f77165183f5c2983cf440af24a
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-17 19:39:41 +0100

    problem with 43.Obadiah.imp

--- bin/test.py
+++ bin/test.py
@@ -2,12 +2,14 @@
 """
 https://zetcode.com/python/jinja/
 """
+import sys
+import re
 from jinja2 import Template, FileSystemLoader, Environment
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
 template = env.get_template('book.xml')
 
-
+"""
 testBook={}
 testBook['name']="Genesis"
 testBook['chapters']=[]
@@ -37,8 +39,43 @@ ch2["verses"][1]["content"]="blablabla 22"
 
 testBook["chapters"].append(ch1)
 testBook["chapters"].append(ch2)
+"""
+
+book={}
+inputFiles=sys.argv[1]
+print( sys.argv)
+for curFileName in sys.argv[1:]:
+  with open(curFileName) as fp:
+    #DO we expect a $$$Name/// header ? 
+    newVerseFlag=True
+    for line in fp:
+      if re.search("^\s$",line):
+        continue
+      #print("line='%s'"%line.strip())
+      if newVerseFlag:
+        print("new")
+        print("line='%s'"%line.strip())
+        verseLineReg=re.search("\$\$\$(\S+)/(\d+)/(\d+)",line)
+        chapterLineReg=re.search("\$\$\$(\S+)/(\d+)",line)
+        if verseLineReg:
+          bookName=verseLineReg.group(1)
+          chapter=verseLineReg.group(2)
+          verse=verseLineReg.group(3)
+          print(bookName,chapter,verse)
+        elif chapterLineReg:
+          bookName=chapterLineReg.group(1)
+          chapter=chapterLineReg.group(2)
+          print(bookName,chapter)
+        else:
+          print("Cannot parse '%s'"%line.strip())
+          sys.exit()
+        newVerseFlag=False
+      else:
+        newVerseFlag=True
+    fp.close()
 
 
 
-output = template.render(book=testBook)
-print(output)
+
+output = template.render(book=book)
+#print(output)

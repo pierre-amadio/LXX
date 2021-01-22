4f83b3fe448618934aec222ec78c4f96cbbf78ae
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-22 22:46:32 +0100

    on avance

--- bin/4-convertToXml.sh
+++ bin/4-convertToXml.sh
@@ -8,11 +8,22 @@ mkdir xml1
 ./bin/imp2xml.py imp/06.Deut.imp > xml1/06.Deut.xml
 ./bin/imp2xml.py imp/07.JoshB.imp > xml1/07.JoshB.xml
 ./bin/imp2xml.py imp/08.JoshA.imp > xml1/08.JoshA.xml
-
+./bin/imp2xml.py imp/09.JudgesB.imp > xml1/09.JudgesB.xml
+./bin/imp2xml.py imp/10.JudgesA.imp > xml1/10.JudgesA.xml
+./bin/imp2xml.py imp/11.Ruth.imp > xml1/11.Ruth.xml
+./bin/imp2xml.py imp/12.1Sam.imp > xml1/12.1Sam.xml
+./bin/imp2xml.py imp/13.2Sam.imp   > xml1/13.2Sam.xml
+./bin/imp2xml.py imp/14.1Kings.imp > xml1/14.1Kings.xml
+./bin/imp2xml.py imp/15.2Kings.imp > xml1/15.2Kings.xml
+./bin/imp2xml.py imp/16.1Chron.imp > xml1/16.1Chron.xml
+./bin/imp2xml.py imp/17.2Chron.imp > xml1/17.2Chron.xml
+./bin/imp2xml.py imp/ > xml1/
+./bin/imp2xml.py imp/ > xml1/
+./bin/imp2xml.py imp/ > xml1/
+./bin/imp2xml.py imp/ > xml1/
 ./bin/imp2xml.py imp/30.Odes.imp > xml1/30.Odes.xml
 exit
 
-./bin/imp2xml.py imp/ > xml1/
 ./bin/imp2xml.py imp/ > xml1/
 ./bin/imp2xml.py imp/ > xml1/
 ./bin/imp2xml.py imp/ > xml1/

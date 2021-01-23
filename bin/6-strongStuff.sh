47e7487734637253f28072d2cae60b170cd371b0
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-23 13:10:40 +0100

    strong processing

--- bin/6-strongStuff.sh
+++ bin/6-strongStuff.sh
@@ -3,6 +3,13 @@
 rm -rf strong
 mkdir strong
 
-./bin/addStrongNbr.py specific/57-Odes.xml ./strong
+for i in `ls specific`; do
+  #echo $i;
+  time ./bin/addStrongNbr.py specific/$i ./strong
+  sleep 1
+done;
+
+
+#./bin/addStrongNbr.py specific/57-Odes.xml ./strong
 #./bin/addStrongNbr.py xml1/08.JoshA.xml ./strong
 

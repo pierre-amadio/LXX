2117d95a771021d83ed8796b11a9aa756a6bb168
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-23 12:20:04 +0100

    adding imp to xml stuff

--- bin/4-convertToXml.sh
+++ bin/4-convertToXml.sh
@@ -19,20 +19,45 @@ mkdir xml1
 ./bin/imp2xml.py imp/17.2Chron.imp > xml1/17.2Chron.xml
 ./bin/imp2xml.py imp/18.1Esdras.imp > xml1/18.1Esdras.xml
 ./bin/imp2xml.py imp/19.2Esdras.imp > xml1/19.2Esdras.imp
-#./bin/imp2xml.py imp/ > xml1/
-#./bin/imp2xml.py imp/ > xml1/
+./bin/imp2xml.py imp/20.Esther.imp > xml1/
+./bin/imp2xml.py imp/21.Judith.imp > xml1/
+./bin/imp2xml.py imp/22.TobitBA.imp > xml1/
+./bin/imp2xml.py imp/23.TobitS.imp > xml1/
+./bin/imp2xml.py imp/24.1Macc.imp > xml1/
+./bin/imp2xml.py imp/25.2Macc.imp > xml1/
+./bin/imp2xml.py imp/26.3Macc.imp > xml1/
+./bin/imp2xml.py imp/27.4Macc.imp > xml1/
+./bin/imp2xml.py imp/28.Psalms1.imp imp/29.Psalms2.imp > xml1/
 ./bin/imp2xml.py imp/30.Odes.imp > xml1/30.Odes.xml
-exit
-
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
-./bin/imp2xml.py imp/ > xml1/
+./bin/imp2xml.py imp/31.Proverbs.imp > xml1/
+./bin/imp2xml.py imp/32.Qoheleth.imp > xml1/
+./bin/imp2xml.py imp/33.Canticles.imp > xml1/
+./bin/imp2xml.py imp/34.Job.imp > xml1/
+./bin/imp2xml.py imp/35.Wisdom.imp > xml1/
+./bin/imp2xml.py imp/36.Sirach.imp > xml1/
+./bin/imp2xml.py imp/37.PsSol.imp > xml1/
+./bin/imp2xml.py imp/38.Hosea.imp > xml1/
+./bin/imp2xml.py imp/39.Micah.imp > xml1/
+./bin/imp2xml.py imp/40.Amos.imp > xml1/
+./bin/imp2xml.py imp/41.Joel.imp > xml1/
+./bin/imp2xml.py imp/42.Jonah.imp > xml1/
+./bin/imp2xml.py imp/43.Obadiah.imp > xml1/
+./bin/imp2xml.py imp/44.Nahum.imp > xml1/
+./bin/imp2xml.py imp/45.Habakkuk.imp > xml1/
+./bin/imp2xml.py imp/46.Zeph.imp > xml1/
+./bin/imp2xml.py imp/47.Haggai.imp > xml1/
+./bin/imp2xml.py imp/48.Zech.imp > xml1/
+./bin/imp2xml.py imp/49.Malachi.imp > xml1/
+./bin/imp2xml.py imp/50.Isaiah1.imp imp/51.Isaiah2.imp> xml1/
+./bin/imp2xml.py imp/52.Jer1.imp imp/53.Jer2.imp> xml1/
+./bin/imp2xml.py imp/54.Baruch.imp > xml1/
+./bin/imp2xml.py imp/55.EpJer.imp > xml1/
+./bin/imp2xml.py imp/56.Lam.imp > xml1/
+./bin/imp2xml.py imp/57.Ezek1.imp imp/58.Ezek2.imp > xml1/
+./bin/imp2xml.py imp/59.BelOG.imp > xml1/
+./bin/imp2xml.py imp/60.BelTh.imp > xml1/
+./bin/imp2xml.py imp/61.DanielOG.imp > xml1/
+./bin/imp2xml.py imp/62.DanielTh.imp > xml1/
+./bin/imp2xml.py imp/63.SusOG.imp > xml1/
+./bin/imp2xml.py imp/64.SusTh.imp > xml1/
 

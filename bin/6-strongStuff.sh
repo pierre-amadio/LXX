f67822f2683f845459218489477758aefb7c1c81
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-24 12:58:57 +0100

    getting rid of the <?xml version=1.0 encoding=utf-8?> line when adding strong

--- bin/6-strongStuff.sh
+++ bin/6-strongStuff.sh
@@ -9,6 +9,8 @@ for i in `ls specific`; do
       echo "strong/$i already exists."
   else
   time ./bin/addStrongNbr.py specific/$i ./strong
+  #lets get rid of the first <?xml version="1.0" encoding="utf-8"?> line
+  sed -i '1d' ./strong/$i
   sleep 1
   fi
 

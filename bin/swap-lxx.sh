dd82df11fe91d5ad78e9bd288b0b1544f402038b
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-25 11:05:46 +0100

    adding bin/swap-lxx.sh

---
+++ bin/swap-lxx.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+installmgr -u LXX
+oldzip=/home/melmoth/dev/LXX/LXX.zip
+
+rm -rf ./tmp
+mkdir ./tmp
+
+if [ $1 = "devel" ]  
+then
+ echo "devel";
+ cp lxx.conf $SWORD_PATH/mods.d
+ mv mod/ $SWORD_PATH/modules/texts/ztext/lxx
+else
+ echo "standard";
+ cp $oldzip ./tmp
+ cd tmp
+ unzip LXX.zip
+ cp mods.d/lxx.conf $SWORD_PATH/mods.d
+ mv modules/texts/ztext/lxx/ $SWORD_PATH/modules/texts/ztext
+fi 
+

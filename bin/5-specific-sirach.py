80b65bfab7e84717962e90340c423923836b998f
Pierre Amadio <amadio.pierre@gmail.com>
2021-01-26 15:08:08 +0100

    dealing with prolog of sirach

---
+++ bin/5-specific-sirach.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Prolog  verses are supposed to be milestone _before_ verse1.
+<chapter osisID="Sir.1">
+ <milestone type="x-alt-v11n" n="1"/>
+  <w lemma="strong:G4183 lex:πολύς" morph="packard:A1+GPN" xlit="betacode:POLLW=N">πολλῶν</w>
+  ...
+ <milestone type="x-alt-v11n" n="2"/>
+   <w lemma="strong:G2532 lex:καί" morph="packard:C" xlit="betacode:KAI\">καὶ</w>
+
+ right now, those are set as milestones in verses Sir1:0
+
+arg1:  input file
+arg2:  output directory
+"""
+import sys
+import re
+from bs4 import BeautifulSoup
+import copy
+
+inputFile=sys.argv[1]
+outputDir=sys.argv[2]
+
+
+def dealWith(myFile):
+  with open(myFile) as fp:
+    soup = BeautifulSoup(fp, 'xml')
+    insertIndex=0 
+    milestoneCnt=1
+    lastNewTag=False
+    for verse in soup.find_all(osisID="Sir.1.0"):
+      #I wonder if it may better to include the prolog before chapter 1, directly under the book <dev> ?
+      chapter=verse.parent
+      milestone=soup.new_tag("milestone",type="x-alt-v11n",n="%s"%milestoneCnt)
+      milestoneCnt+=1
+      chapter.insert(insertIndex,milestone)
+      insertIndex+=1
+      for c in verse.children:
+        newC=copy.copy(c)
+        chapter.insert(insertIndex,newC)
+        insertIndex+=1
+      verse.decompose()
+    return str(soup)
+
+
+print("Dealing with Prolog of Sirach")
+
+newFile="%s/%s"%(outputDir,"32-Sir.xml")
+
+newXml=dealWith(inputFile)
+
+
+with open(newFile, "w", encoding='utf-8') as file:
+  file.write(newXml)
+  print(newFile)
+
+
--- bin/5-specificStuff.sh
+++ bin/5-specificStuff.sh
@@ -44,7 +44,8 @@ cp xml1/32.Qoheleth.xml specific/28-Eccl.xml
 cp xml1/33.Canticles.xml specific/29-Song.xml
 cp xml1/34.Job.xml specific/30-Job.xml
 cp xml1/35.Wisdom.xml specific/31-Wis.xml
-cp xml1/36.Sirach.xml specific/32-Sir.xml
+#cp xml1/36.Sirach.xml specific/32-Sir.xml
+./bin/5-specific-sirach.py xml1/36.Sirach.xml  specific/
 cp xml1/37.PsSol.xml specific/33-PssSol.xml
 cp xml1/38.Hosea.xml specific/34-Hos.xml
 cp xml1/40.Amos.xml specific/35-Amos.xml

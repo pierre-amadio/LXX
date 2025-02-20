4b6f665b8e33980749a819e600bb59a6ed73c38d
Pierre Amadio <pierre.amadio@libertysurf.fr>
2025-02-20 21:33:27 +0100

    missing verses

---
+++ bin/4-empty-verses.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Odes miss some verses that confuses reader.
+Lets fill in empty verses where there are one.
+
+Then some title should be moved from a verse node to the parent chapter node.
+
+arg1:  input file
+arg2:  output directory
+"""
+import sys
+import re
+from bs4 import BeautifulSoup
+
+inputFile=sys.argv[1]
+outputDir=sys.argv[2]
+
+
+def missingVersesFromFile(fileName):
+  with open(fileName) as fp:
+    soup = BeautifulSoup(fp, 'xml')
+
+    curChapter=0
+    bookName=None
+    for b in soup.find_all("div",type="book"):
+      bookName=b["osisID"]
+
+    if bookName==None:
+      sys.exit("Cannot find book name")
+
+    for chapter in soup.find_all('chapter'):
+      curVerseNbr=0
+      rc=re.search("%s\.(\d+)"%bookName,chapter["osisID"])
+      if rc:
+        curChapter=int(rc.group(1))
+      else:
+        print("Cannot parse chapter %s"%chapter["osisID"])
+        sys.exit()
+
+      for verse in chapter.find_all("verse"):
+        expectedVerseNbr=curVerseNbr+1
+        snt="%s.%s.(\d+)"%(bookName,curChapter)
+        rv=re.search(snt,verse["osisID"])
+        if rv:
+          curVerseNbr=int(rv.group(1))
+        else:
+          print("Cannot parse verse %s"%verse)
+          sys.exit()
+
+        if curVerseNbr!=expectedVerseNbr:
+            for missing in range (expectedVerseNbr,curVerseNbr):
+              newVerseTag=soup.new_tag("verse", osisID="%s.%s.%s"%(bookName,curChapter,missing))
+              newVerseTag.string=" "
+              verse.insert_before(newVerseTag)
+  return str(soup)
+
+
+print("Dealing with %s"%inputFile)
+newFile="%s/%s"%(outputDir,inputFile)
+allVersesXml=missingVersesFromFile("xml0/%s"%inputFile)
+
+with open(newFile, "w", encoding='utf-8') as file:
+  file.write(allVersesXml)
+
--- bin/5-specific-odes.py
+++ bin/5-specific-odes.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Odes miss some verses that confuses reader.
-Lets fill in empty verses where there are one.
-
-Then some title should be moved from a verse node to the parent chapter node.
+Some title should be moved from a verse node to the parent chapter node.
 
 arg1:  input file
 arg2:  output directory
@@ -16,52 +13,20 @@ from bs4 import BeautifulSoup
 inputFile=sys.argv[1]
 outputDir=sys.argv[2]
 
-
-def missingVersesFromFile(fileName):
+def moveTitle(fileName):
   with open(fileName) as fp:
     soup = BeautifulSoup(fp, 'xml')
-
     curChapter=0
-    for chapter in soup.find_all('chapter'):
-      curVerseNbr=0
-      rc=re.search("Odes\.(\d+)",chapter["osisID"])
-      if rc:
-        curChapter=int(rc.group(1))
-      else:
-        print("Cannot parse chapter %s"%chapter["osisID"])
-        sys.exit()
+    for title in soup.find_all('title'):
+      verse=title.parent
+      chapter=verse.parent
+      cur=title.extract()
+      chapter.insert(0,title)
 
-      for verse in chapter.find_all("verse"):
-        expectedVerseNbr=curVerseNbr+1
-        snt="Odes.%s.(\d+)"%curChapter
-        rv=re.search(snt,verse["osisID"])
-        if rv:
-          curVerseNbr=int(rv.group(1))
-        else:
-          print("Cannot parse verse %s"%verse)
-          sys.exit()
-
-        if curVerseNbr!=expectedVerseNbr:
-            for missing in range (expectedVerseNbr,curVerseNbr):
-              newVerseTag=soup.new_tag("verse", osisID="Odes.%s.%s"%(curChapter,missing))
-              newVerseTag.string=" "
-              verse.insert_before(newVerseTag)
-  return str(soup)
-
-def moveTitle(origXml):
-  soup = BeautifulSoup(origXml, 'xml')
-  curChapter=0
-  for title in soup.find_all('title'):
-    verse=title.parent
-    chapter=verse.parent
-    cur=title.extract()
-    chapter.insert(0,title)
-
-  return str(soup)
+    return str(soup)
 
 newFile="%s/%s"%(outputDir,"57-Odes.xml")
-allVersesXml=missingVersesFromFile(inputFile)
-newXml=moveTitle(allVersesXml)
+newXml=moveTitle(inputFile)
 
 print("Dealing with Odes")
 with open(newFile, "w", encoding='utf-8') as file:

faefba4232ee3325a04210db2ec33ef8d4628164
Pierre Amadio <pierre.amadio@libertysurf.fr>
2025-02-22 19:46:46 +0100

    8-concatenate.sh

---
+++ bin/8-concatenate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cat <<EOF > lxx.osis.xml 
+<?xml version="1.0" encoding="UTF-8" ?>
+<osis xmlns="http://www.bibletechnologies.net/2003/OSIS/namespace" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.bibletechnologies.net/2003/OSIS/namespace http://www.crosswire.org/~dmsmith/osis/osisCore.2.1.1-cw-latest.xsd">
+<osisText osisIDWork="LXX" osisRefWork="OT" xml:lang="grc">
+	<header>
+	      <revisionDesc resp="cyrille">
+        <date>2025.02.20T22.15.53</date>
+        <p>Converted from lxxm.imp</p>
+       </revisionDesc>
+		<work osisWork="LXX">
+			<title>Septuagint, Morphologically Tagged Rahlfs'</title>
+			<identifier type="OSIS">Bible.LXX</identifier>
+	  </work>
+		<work osisWork="LXX">
+			<refSystem>Bible.LXX</refSystem>
+		</work>
+	</header>
+EOF
+
+for i in `ls pilcrow/` ; do 
+ echo $i;
+ cat pilcrow/$i >> lxx.osis.xml
+done
+
+echo  "</osisText></osis>">>lxx.osis.xml

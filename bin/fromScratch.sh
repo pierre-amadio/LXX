bb625dd7950a5a8b4b70a1048019591082b2dc02
Pierre Amadio <pierre.amadio@libertysurf.fr>
2025-02-23 11:06:16 +0100

    fromScratch

--- bin/fromScratch.sh
+++ bin/fromScratch.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #installmgr -u LXX
 #echo yes |installmgr -ri CrossWire LXX
+rm -rf original-text imp mod pilcrow specific strong xml0 xml1 tmp
 ./bin/2-downloadRawText.sh
 ./bin/3-convertToImp.sh
 ./bin/4-convertToXml.sh
 ./bin/5-specificStuff.sh
 ./bin/6-strongStuff.sh
-./bin/7-concatenate.sh
+./bin/7-marking-concatenated-verses.sh
+./bin/8-concatenate.sh
+
 

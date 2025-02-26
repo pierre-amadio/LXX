194605e14529eeaa65dca8e5034a1449baad6f8c
Pierre Amadio <pierre.amadio@libertysurf.fr>
2025-02-26 15:09:29 +0100

    fromScratch

--- bin/fromScratch.sh
+++ bin/fromScratch.sh
@@ -5,9 +5,11 @@ rm -rf original-text imp mod pilcrow specific strong xml0 xml1 tmp
 ./bin/2-downloadRawText.sh
 ./bin/3-convertToImp.sh
 ./bin/4-convertToXml.sh
-./bin/5-specificStuff.sh
-./bin/6-strongStuff.sh
-./bin/7-marking-concatenated-verses.sh
-./bin/8-concatenate.sh
+./bin/5-sort-verses.sh 
+./bin/6-missing-verses.sh 
+./bin/7-specificStuff.sh 
+./bin/8-strongStuff.sh 
+./bin/9-marking-concatenated-verses.sh
+./bin/10-concatenate.sh
 
 
